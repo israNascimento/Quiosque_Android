1b7be6ab8ac906b8bf789f9d8ffa92ad0259a993
Israel Nascimento <dev.israelnascimento@gmail.com>
2017-06-10 15:39:28 -0300

    Recebendo resposta do servidor. Adicionado txt para auxiliar o desenvolvimento com as novidades

--- app/src/main/java/ufrrj/com/quiosque/MainActivity.java
+++ app/src/main/java/ufrrj/com/quiosque/MainActivity.java
@@ -2,12 +2,40 @@ package ufrrj.com.quiosque;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import ufrrj.com.quiosque.web.LoginTask;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button signin;
+    private EditText matricula, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        matricula = (EditText) findViewById(R.id.login_matricula);
+        password = (EditText) findViewById(R.id.login_password);
+        signin = (Button) findViewById(R.id.login_signin);
+        signin.setOnClickListener(this);
+    }
+
+    private void Login(String matricula, String password) {
+        Log.d("Login", "Matricula: "+matricula+" Senha: "+password);
+        new LoginTask().execute(matricula, password);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.login_signin:
+                Login(matricula.getText().toString(), password.getText().toString());
+                break;
+        }
     }
 }
---
+++ app/src/main/java/ufrrj/com/quiosque/web/LoginTask.java
@@ -0,0 +1,78 @@
+package ufrrj.com.quiosque.web;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by israel on 10/06/17.
+ */
+
+public class LoginTask extends AsyncTask<String, Void, String> {
+
+
+    @Override
+    protected String doInBackground(String... params) {
+        String matricula = params[0];
+        String senha = params[1];
+
+        try {
+            URL url = new URL("http://academico.ufrrj.br/quiosque/aluno/quiosque.php");
+            Log.d("Web", "Conectando...");
+            return Login(url, matricula, senha);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    String Login(URL url, String matricula, String senha) {
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+
+            String conteudoPOST = "edtIdUs=2016390271&edtIdSen=15020876771&btnIdOk=+Ok+";
+
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Length", ""+conteudoPOST.getBytes().length);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            Log.d("Web", ""+connection.getRequestProperties().toString());
+
+            DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+            byte[] bytes = conteudoPOST.getBytes("UTF-8");
+            dataOutputStream.write(bytes, 0, bytes.length);
+            dataOutputStream.flush();
+            dataOutputStream.close();
+
+            connection.connect();
+
+            Scanner scanner = new Scanner(connection.getInputStream());
+            StringBuilder builder = new StringBuilder();
+
+            while(scanner.hasNextLine()) {
+                builder.append(scanner.nextLine());
+            }
+            Log.d("Web", builder.toString());
+            connection.disconnect();
+            return builder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
